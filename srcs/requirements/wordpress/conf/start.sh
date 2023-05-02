bf602359bc98444d1781a698710edd3dd4da2671
Dmarceli <daniel.sequeira@skeyra.pt>
2023-05-02 14:36:08 +0100

    still no webpage

--- srcs/requirements/wordpress/conf/start.sh
+++ srcs/requirements/wordpress/conf/start.sh
@@ -15,11 +15,16 @@ cd wordpress/
 
 cp /wp-config.php .
 
-
 sed -i -r "s/db1/$db_name/1"   wp-config.php
 sed -i -r "s/user/$db_user/1"  wp-config.php
 sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
 
+chmod -R 775 /var/www/
+chown -R www-data:www-data /var/www/
 mkdir /run/php
+
+touch /run/php/php7.3-fpm.pid
+rm /etc/php/7.3/fpm/pool.d/www.conf
+cp /www.conf /etc/php/7.3/fpm/pool.d
 
 php-fpm7.3 -F

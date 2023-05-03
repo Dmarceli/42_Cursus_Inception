100808a9bf7de1f0af36bac90982f2d1d8db2aca
Dmarceli <daniel.sequeira@skeyra.pt>
2023-05-03 21:38:15 +0100

    persistance working, still left db dump and second user

--- srcs/requirements/mariadb/tools/makedb.sql
+++ srcs/requirements/mariadb/tools/makedb.sql
@@ -1,7 +1,5 @@
 CREATE DATABASE IF NOT EXISTS marydb;
 
---select User , Host from mysql.user ;
-
 CREATE USER IF NOT EXISTS 'teste'@'%' IDENTIFIED BY 'password';
 
 GRANT ALL PRIVILEGES ON *.* TO 'teste'@'%' WITH GRANT OPTION;
